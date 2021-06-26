40cfeb8aa74d4a56788bb290b21667217ef1b859
Kim Lindhard <kim.lindhard@gmail.com>
2021-06-26 22:25:53 +0200

    push random image into images_scheduled

--- assemblies/controller/src/features/rotating-images/feature-scedule.js
+++ assemblies/controller/src/features/rotating-images/feature-scedule.js
@@ -11,35 +11,46 @@ const registerQueue = (registerAction) =>  registerAction({
     ]
 });
 
+const getImageUrl = () => {
 
-const workerHandler = async (doc, context) => {
+    const images = [
+        'https://pixabay.com/get/gdd488fc242a6fc24a3c5772f098a9111e06dcd43bf0ec2ef9b491ad4838b92ca8a7a49e38a9afcf7811c117e00099bfa_1280.jpg',
+        'https://pixabay.com/get/g53241e98a59fbd04ce6b364a67739d94d3719f6c7163dd92d97f2aed6bc1ca8526e2f30e1106af15fe0cf5c7d0d88c5b_1280.jpg',
+        'https://pixabay.com/get/gb134d59cddfbb04339ed594e75bd0d422064be318286c43205c5e4f02fbd076a11f4d84d5521d8c39aa0890e1f3698d9_1280.jpg',
+        'https://pixabay.com/get/g6ec5aefbb8875b17125f2398e62346396c4b22f361e0f7c78731c905451152fa7e72b06c32183ecc5c1bc037e49bfccf_1280.jpg',
+        'https://pixabay.com/get/geff085c7dee1aaf9397140519d6706c735b37e497e30786387d1d2c0d893321338f3e3d568a261545c54a16efce33b84_1280.jpg',
+        'https://pixabay.com/get/g133101f41f04bf849865356dbc366f91622e21811e30ff723640382915a3b4061a806fd94bf7a77c20823e8fa6daa547_1280.jpg',
+        'https://pixabay.com/get/geee9ff3c51d35de5227ec3a78d54ba2aa84fb0146705be573f490116d9e53750b57159de9cc34e3f1f08e8d9e0097dfe_1280.jpg',
+        'https://pixabay.com/get/g4d53a1068189bd6f6612306da16924d0b09e912ef8b9986cb7957256483385b7c1a454bbc7180b1d15de7a81167588f3_1280.jpg'
+    ]
+    const image = images[Math.floor(Math.random() * images.length)] 
+return image;
+}
+
+const scheduleImageInFiveProcess = async (doc, context) => {
 
     const fetchq = context.client;
     console.log(context.name);
-    const results = await fetchq.pool.query('SELECT * FROM NOW()');
-    // DO DB MAGIC HERE
-    // insert into images_scheduled (url, dont_show_before) values ('aaa',current_timestamp + (5 ||' minutes')::interval)
 
-    console.log(results.rows[0]);
+    await fetchq.pool.query(`insert into images_scheduled (url, dont_show_before) values ('${getImageUrl()}',current_timestamp + (5 ||' minutes')::interval)`);
+
+    console.log(results);
 
     return doc.reschedule("+5s");
   };
 
 
-const registerWorker = (registerAction, getContext) => registerAction({
+const registerWorker = (registerAction) => registerAction({
     hook: "$FETCHQ_REGISTER_WORKER",
     name: featureName,
     trace: __filename,
-    handler: (_, { getContext }) => {
-        const fetchq = getContext("fetchq");
-
-
+    handler: () => {
         return [
-        {
-            queue: scheduleImagesQueue,
-            handler: workerHandler,
-            decorateContext: {name: "Kim"}
-        },
+            {
+                queue: scheduleImagesQueue,
+                handler: scheduleImageInFiveProcess,
+                decorateContext: {name: "Kim"}
+            },
         ];
     },
 });
@@ -63,7 +74,7 @@ const startFeature = (registerAction, getContext) => registerAction({
 module.exports = ({ registerAction, getContext }) => {
 
     registerQueue(registerAction);
-    registerWorker(registerAction, getContext);
+    registerWorker(registerAction);
     startFeature(registerAction,getContext);
 
   };
