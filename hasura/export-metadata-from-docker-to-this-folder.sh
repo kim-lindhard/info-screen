db7fa21fb26e04b570bb7db81eecf0bb3d098fd9
Kim Lindhard <kim.lindhard@gmail.com>
2021-08-24 15:30:36 +0200

    add export hasura meta data script

--- hasura/export-metadata-from-docker-to-this-folder.sh
+++ hasura/export-metadata-from-docker-to-this-folder.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 docker exec info-screen_hasura_1 hasura-cli init /home/hasura
 docker exec info-screen_hasura_1 hasura-cli metadata export --project /home/hasura
-docker cp $(docker ps -aqf "name=info-screen_hasura_1"):/home/hasura/metadata ./metadata
+docker cp info-screen_hasura_1:/home/hasura/metadata/ ./
