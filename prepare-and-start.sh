702e7642fafa20970a054144cdd0ccf256a4ab44
Kim Lindhard <kim.lindhard@gmail.com>
2021-09-01 09:31:11 +0200

    controller connects to database

--- prepare-and-start.sh
+++ prepare-and-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Installing node modules (this can take a little time before you see anything)"
 mkdir -p .docker-data/website/node_modules
-docker-compose -f docker-compose-install-website-node-modules.yaml up
+docker-compose -f docker-compose-install-node-modules.yaml up
 docker-compose -f docker-compose-prepare.yaml -f docker-compose.yaml up -d
